3ebeb38e2c6e88d6aa586b955292d153b197f573
kota <kokota.ohara@gmail.com>
2023-03-12 20:00:28 +0900

    ダイアログサンプル追加

---
+++ src/components/Dialog.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+interface Props {
+  body: string
+}
+const Props = defineProps<Props>()
+const dialog = ref<boolean>(false)
+</script>
+
+<template>
+  <div>
+    <v-dialog
+      v-model="dialog"
+      width="auto"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn v-bind="props" icon="mdi-dots-horizontal" flat size="small" />
+      </template>
+      <v-card min-height="200px">
+        <v-card-title>
+          タイトル
+        </v-card-title>
+        <v-card-subtitle>
+          サブタイトル
+        </v-card-subtitle>
+        <v-card-text class="pb-3">
+          {{ body }}
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
